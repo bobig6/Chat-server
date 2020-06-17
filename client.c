9cda20615109b41789b540a6cea016e2a47d42dc
bobig6 <b.i.ivanov1@gmail.com>
2020-06-17 18:17:07 +0300

    Adding chat rooms

--- client.c
+++ client.c
@@ -18,6 +18,7 @@
 volatile sig_atomic_t flag = 0;
 int sockfd = 0;
 char name[NAME_LEN];
+char groupName[NAME_LEN];
 
 //function for UI in console
 void str_overwrite_stdout(){
@@ -97,6 +98,28 @@ void send_thread(){
 }
 
 
+//monitors sending messages to group
+void send_to_group_thread(){
+    char message[BUFFER_SZ];
+
+    while (1)
+    {
+        //gets message
+        printf("Enter message:");
+        str_overwrite_stdout();
+        fgets(message, BUFFER_SZ, stdin);
+        remove_new_line(message, BUFFER_SZ);
+
+        //send message to server
+        send(sockfd, message, strlen(message), 0);
+        bzero(message, BUFFER_SZ);
+
+    }
+    exits_the_program(2);
+}
+
+
+
 int main(int argc, char **argv){
 	
 
@@ -104,6 +127,9 @@ int main(int argc, char **argv){
 	int port = 4444;
 
     signal(SIGINT, exits_the_program);
+
+    
+
     //gets name and sends it to the server
     printf("Enter your name: ");
     fgets(name, NAME_LEN, stdin);
@@ -130,18 +156,79 @@ int main(int argc, char **argv){
     //sending name to server
     send(sockfd, name, NAME_LEN, 0);
 
-    //creating the thread to send messages
-    pthread_t send_msg_thread;
-    if(pthread_create(&send_msg_thread, NULL, (void*) send_thread, NULL) != 0){
-        perror("pthread_create");
-        return EXIT_FAILURE;
+    while (1)
+    {
+        char option[NAME_LEN];
+        printf("Choose an option: \n");
+        printf("c - create group \n");
+        printf("j - join group \n");
+        printf("m - message person \n");
+        fgets(option, NAME_LEN, stdin);
+        remove_new_line(option, strlen(option));
+
+        if(strcmp(option, "c") == 0){
+            //creates server
+            char command[NAME_LEN] = "create";
+            send(sockfd, command, NAME_LEN, 0);
+            printf("Choose group name: \n");
+            fgets(groupName, NAME_LEN, stdin);
+            remove_new_line(groupName, strlen(groupName));
+            if(strlen(groupName) > NAME_LEN - 1 || strlen(groupName) < 2){
+                printf("Enter group name correctly\n");
+                return EXIT_FAILURE;
+            }
+            send(sockfd, groupName, NAME_LEN, 0);
+            pthread_t send_group_thread;
+            if(pthread_create(&send_group_thread, NULL, (void*) send_to_group_thread, NULL) != 0){
+                perror("pthread_create");
+                return EXIT_FAILURE;
+            }
+            break;
+        }else if (strcmp(option, "j") == 0)
+        {
+            //joins created server
+            char command[NAME_LEN] = "join";
+            send(sockfd, command, NAME_LEN, 0);
+            printf("Enter group name: \n");
+            fgets(groupName, NAME_LEN, stdin);
+            remove_new_line(groupName, strlen(groupName));
+            if(strlen(groupName) > NAME_LEN - 1 || strlen(groupName) < 2){
+                printf("Enter group name correctly\n");
+                return EXIT_FAILURE;
+            }
+            send(sockfd, groupName, NAME_LEN, 0);
+            pthread_t send_group_thread;
+            if(pthread_create(&send_group_thread, NULL, (void*) send_to_group_thread, NULL) != 0){
+                perror("pthread_create");
+                return EXIT_FAILURE;
+            }
+            break;
+        }
+        else if (strcmp(option, "m") == 0)
+        {
+            //messages to other people
+            char command[NAME_LEN] = "message";
+            send(sockfd, command, NAME_LEN, 0);
+            //creating the thread to send messages
+            pthread_t send_msg_thread;
+            if(pthread_create(&send_msg_thread, NULL, (void*) send_thread, NULL) != 0){
+                perror("pthread_create");
+                return EXIT_FAILURE;
+            }
+            break;
+        }else{
+            printf("Didn't get that try again!");
+        }
+        
     }
+    
     //creating the thread to monitor the received messages
     pthread_t recv_msg_thread;
     if(pthread_create(&recv_msg_thread, NULL, (void*) receive_thread, NULL) != 0){
         perror("pthread_create");
         return EXIT_FAILURE;
     }
+    
 
     while(1){
         if(flag){
--- server.c
+++ server.c
@@ -12,11 +12,14 @@
 #include <strings.h>
 
 #define MAX_CLIENTS 100
+#define MAX_GROUPS 100
 #define BUFFER_SZ 2048
 #define NAME_LEN 32
 
 static int cli_count = 0;
 static int uid = 10;
+
+
 
 //Struct to hold client information
 typedef struct
@@ -27,9 +30,20 @@ typedef struct
     char name[NAME_LEN];
 } client_t;
 
+//Struct to hold groups information
+typedef struct
+{
+    char name[NAME_LEN];
+    client_t *clients[MAX_CLIENTS];
+    int last_client;
+} group_t;
+
+
 client_t *clients[MAX_CLIENTS];
+group_t *groups[MAX_CLIENTS];
 
 pthread_mutex_t client_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t groups_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
 //Adds client to clients array
@@ -60,6 +74,66 @@ void queue_remove(int uid){
     pthread_mutex_unlock(&client_mutex);
 }
 
+
+//Adds group to groups array
+void add_group(char name[NAME_LEN]){
+    pthread_mutex_lock(&groups_mutex);
+    for(int i = 0; i < MAX_GROUPS; i++){
+        if(!groups[i]){
+            group_t *gr = (group_t *)malloc(sizeof(group_t));
+            strcpy(gr->name, name);
+            gr->last_client = 0;
+            groups[i] = gr;
+            break;
+        }
+    }
+    pthread_mutex_unlock(&groups_mutex);
+}
+
+//Adds user to group array
+void add_user_to_group(client_t *cl, char name[NAME_LEN]){
+    pthread_mutex_lock(&groups_mutex);
+    for(int j = 0; j < MAX_GROUPS; j++){
+        if(groups[j]){
+            if(strcmp(groups[j]->name, name) == 0){
+                for(; groups[j]->last_client < MAX_CLIENTS;){
+                    if(groups[j]->clients[groups[j]->last_client]->name != NULL){}
+                    groups[j]->clients[groups[j]->last_client] = cl;
+                    printf("%s joined %s with id %d\n", groups[j]->clients[groups[j]->last_client]->name, groups[j]->name, groups[j]->last_client);
+                    groups[j]->last_client++;
+                    break;
+                    
+                }
+            }
+        }
+    }
+    pthread_mutex_unlock(&groups_mutex);
+}
+
+
+//remove client from group
+void remove_user_from_group(int uid, char name[NAME_LEN]){
+    pthread_mutex_lock(&groups_mutex);
+
+    for(int j = 0; j < MAX_GROUPS; j++){
+        for(int i = 0; i < MAX_CLIENTS; i++){
+            if(groups[j]){
+                if(strcmp(groups[j]->name, name) == 0){
+                    if(!groups[j]->clients[i]){
+                        if(groups[j]->clients[i]->uid == uid){
+                            clients[i] = NULL;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    pthread_mutex_unlock(&groups_mutex);
+}
+
+
 //function to send message to a user
 void send_message(char *username, char *s){
     int is_sent = 0;
@@ -83,6 +157,44 @@ void send_message(char *username, char *s){
 }
 
 
+
+//function to send message to a group
+void send_message_to_group(char *group_name, char *s, client_t *cli){
+    int is_sent = 0;
+    pthread_mutex_lock(&groups_mutex);
+
+    for(int j = 0; j < MAX_GROUPS; j++){
+        if(groups[j]){
+            if(strcmp(groups[j]->name, group_name) == 0){
+                for(int i = 0; i < groups[j]->last_client; i++){
+                    if(groups[j]->clients[i]){
+                        if(groups[j]->clients[i]->uid != cli->uid){
+                            char *message = malloc(sizeof(char)* 1000);
+                            sprintf(message, "%s> %s", cli->name, s);
+                            if(write(groups[j]->clients[i]->sockfd, message, strlen(message)) < 0){
+                                perror("ERROR: write to descriptor failed");
+                                break;
+                            }
+                            is_sent = 1;
+                        }
+                    }
+                }
+        
+            }
+
+        }
+    }
+
+    if(is_sent == 0){
+        printf("%s doesn't exist\n", group_name);
+    }
+    pthread_mutex_unlock(&groups_mutex);
+}
+
+
+
+
+
 //thread function for receiving from every client
 void *clientThread(void *arg){
     char buffer[BUFFER_SZ];
@@ -100,56 +212,220 @@ void *clientThread(void *arg){
         printf("Name entered corectly\n");
     }
 
+    char option[100];
 
-    while(1){
-        if(leave_flag){
-            break;
+    //getting option
+    int receveOption = recv(cli->sockfd, buffer, BUFFER_SZ, 0);
+    if(receveOption > 0){
+        if(strlen(buffer) > 0){
+            //gets recever             
+            strcpy(option, buffer);
         }
-        char recever[100], message[100];
+    }else if (receveOption == 0 || strcmp(buffer, "exit") == 0)
+    {
+        close(cli->sockfd);
+        queue_remove(cli->uid);
+        free(cli);
+        cli_count--;
+        pthread_detach(pthread_self());
+        return NULL;
+    }else
+    {
+        perror("server");
+        close(cli->sockfd);
+        queue_remove(cli->uid);
+        free(cli);
+        cli_count--;
+        pthread_detach(pthread_self());
 
-        //getting receiver name
-        int receveRecever = recv(cli->sockfd, buffer, BUFFER_SZ, 0);
-        if(receveRecever > 0){
+        return NULL;
+    }
+    bzero(buffer, BUFFER_SZ);
+
+    if(strcmp(option, "create") == 0){
+        char group_name[100];
+        int receveName = recv(cli->sockfd, buffer, BUFFER_SZ, 0);
+        if(receveName > 0){
             if(strlen(buffer) > 0){
                 //gets recever             
-                strcpy(recever, buffer);
+                strcpy(group_name, buffer);
             }
-        }else if (receveRecever == 0 || strcmp(buffer, "exit") == 0)
+        }else if (receveName == 0)
         {
-            leave_flag = 1;
+            close(cli->sockfd);
+            queue_remove(cli->uid);
+            free(cli);
+            cli_count--;
+            pthread_detach(pthread_self());
+            return NULL;
         }else
         {
             perror("server");
-            leave_flag = 1;
+            close(cli->sockfd);
+            queue_remove(cli->uid);
+            free(cli);
+            cli_count--;
+            pthread_detach(pthread_self());
+            return NULL;
         }
         bzero(buffer, BUFFER_SZ);
+        add_group(group_name);
+        add_user_to_group(cli, group_name);
+
+        //send messages
+        while(1){
+            if(leave_flag){
+                break;
+            }
+            char message[100];
+
+            //getting message to send
+            int receveMessage = recv(cli->sockfd, buffer, BUFFER_SZ, 0);
+            if(receveMessage > 0){
+                if(strlen(buffer) > 0){
+                    //gets message            
+                    strcpy(message, buffer);
+
+                }
+            }else if (receveMessage == 0 || strcmp(buffer, "exit") == 0)
+            {
+                leave_flag = 1;
+            }else
+            {
+                perror("server");
+                leave_flag = 1;
+            }
+            bzero(buffer, BUFFER_SZ);
 
 
+            //sends message to recever
+            send_message_to_group(group_name, message, cli);
+
+        }
+        remove_user_from_group(cli->uid, group_name);
 
 
-        //getting message to send
-        int receveMessage = recv(cli->sockfd, buffer, BUFFER_SZ, 0);
-        if(receveMessage > 0){
+    }else if (strcmp(option, "join") == 0)
+    {
+        //get group name
+        char group_name[100];
+        int receveName = recv(cli->sockfd, buffer, BUFFER_SZ, 0);
+        if(receveName > 0){
             if(strlen(buffer) > 0){
-                //gets message            
-                strcpy(message, buffer);
-
+                //gets group             
+                strcpy(group_name, buffer);
             }
-        }else if (receveMessage == 0 || strcmp(buffer, "exit") == 0)
+        }else if (receveName == 0)
         {
-            leave_flag = 1;
+            close(cli->sockfd);
+            queue_remove(cli->uid);
+            free(cli);
+            cli_count--;
+            pthread_detach(pthread_self());
+            return NULL;
         }else
         {
             perror("server");
-            leave_flag = 1;
+            close(cli->sockfd);
+            queue_remove(cli->uid);
+            free(cli);
+            cli_count--;
+            pthread_detach(pthread_self());
+            return NULL;
         }
         bzero(buffer, BUFFER_SZ);
+        add_user_to_group(cli, group_name);
+
+        //send messages
+        while(1){
+            if(leave_flag){
+                break;
+            }
+            char message[100];
+
+            //getting message to send
+            int receveMessage = recv(cli->sockfd, buffer, BUFFER_SZ, 0);
+            if(receveMessage > 0){
+                if(strlen(buffer) > 0){
+                    //gets message            
+                    strcpy(message, buffer);
+
+                }
+            }else if (receveMessage == 0 || strcmp(buffer, "exit") == 0)
+            {
+                leave_flag = 1;
+            }else
+            {
+                perror("server");
+                leave_flag = 1;
+            }
+            bzero(buffer, BUFFER_SZ);
 
 
-        //sends message to recever
-        send_message(recever, message);
+            //sends message to recever
+            send_message_to_group(group_name, message, cli);
 
+        }
+        remove_user_from_group(cli->uid, group_name);
+
+    }else if (strcmp(option, "message") == 0)
+    {
+        while(1){
+            if(leave_flag){
+                break;
+            }
+            char recever[100], message[100];
+
+            //getting receiver name
+            int receveRecever = recv(cli->sockfd, buffer, BUFFER_SZ, 0);
+            if(receveRecever > 0){
+                if(strlen(buffer) > 0){
+                    //gets recever             
+                    strcpy(recever, buffer);
+                }
+            }else if (receveRecever == 0 || strcmp(buffer, "exit") == 0)
+            {
+                leave_flag = 1;
+            }else
+            {
+                perror("server");
+                leave_flag = 1;
+            }
+            bzero(buffer, BUFFER_SZ);
+
+
+
+
+            //getting message to send
+            int receveMessage = recv(cli->sockfd, buffer, BUFFER_SZ, 0);
+            if(receveMessage > 0){
+                if(strlen(buffer) > 0){
+                    //gets message            
+                    strcpy(message, buffer);
+
+                }
+            }else if (receveMessage == 0 || strcmp(buffer, "exit") == 0)
+            {
+                leave_flag = 1;
+            }else
+            {
+                perror("server");
+                leave_flag = 1;
+            }
+            bzero(buffer, BUFFER_SZ);
+
+
+            //sends message to recever
+            send_message(recever, message);
+
+        }
+        
     }
+    
+
+
+
+
     close(cli->sockfd);
     queue_remove(cli->uid);
     free(cli);
